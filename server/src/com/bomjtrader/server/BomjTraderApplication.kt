32b2b4f0125020c8ea1b3eff32f72746583220e2
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2025-07-04 14:30:38 +0200

    Initial commit

---
+++ server/src/com/bomjtrader/server/BomjTraderApplication.kt
@@ -0,0 +1,11 @@
+package com.bomjtrader.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BomjTraderApplication
+
+fun main(args: Array<String>) {
+    runApplication<BomjTraderApplication>()
+}
