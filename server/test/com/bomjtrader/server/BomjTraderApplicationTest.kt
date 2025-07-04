bb095b6aa85b77a2c7db8be9597b014f7d7f30ef
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2025-07-04 17:19:29 +0200

    Add basic test for context loading

---
+++ server/test/com/bomjtrader/server/BomjTraderApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.bomjtrader.server
+
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
+
+@SpringBootTest
+class BomjTraderApplicationTest {
+
+  @Test
+  fun contextLoads() {
+
+  }
+}
